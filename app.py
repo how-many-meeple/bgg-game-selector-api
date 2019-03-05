3fd9a669b41c0bcd12c58cb8f236bd12565ad37a
Teqqles <david.long@racing-leagues.co.uk>
2019-03-05 16:29:25 +0000

    Add geek list lookup and general refactoring

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import json
 from flask import Flask, request
 from flask_cors import CORS
 
-from boardgame.BoardGame import BoardGameFactory, BoardGameUserNotFoundError
-from boardgame.Filter import Filter
-from boardgame.FilterProcessor import FilterProcessor
+from boardgame.board_game import BoardGameFactory, BoardGameUserNotFoundError
+from boardgame.filter import Filter
+from boardgame.filter_processor import FilterProcessor
 
 application = Flask(__name__)
 app = application  # aliased application for convenience
@@ -15,9 +15,21 @@ CORS(app)
 @app.route("/collection/<string:username>")
 def show_games_in_collection(username):
     try:
-        selector = BoardGameFactory.create_selector(username)
-        gfilter = FilterProcessor(Filter.create_filter_chain(request.headers))
-        games = selector.get_games_matching_filter(gfilter)
+        selector = BoardGameFactory.create_player_selector(username)
+        game_filter = FilterProcessor(Filter.create_filter_chain(request.headers))
+        games = selector.get_games_matching_filter(game_filter)
+
+        return json.dumps([game.data() for game in games])
+    except BoardGameUserNotFoundError as error:
+        return error.message, 404
+
+
+@app.route("/geeklist/<geek_list>")
+def show_games_in_list(geek_list):
+    try:
+        selector = BoardGameFactory.create_list_selector(geek_list)
+        game_filter = FilterProcessor(Filter.create_filter_chain(request.headers))
+        games = selector.get_games_matching_filter(game_filter)
 
         return json.dumps([game.data() for game in games])
     except BoardGameUserNotFoundError as error:
