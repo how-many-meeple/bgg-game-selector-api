dd556cfe1ef75a1a1d005df71e08cfda26b0c934
Teqqles <david.long@racing-leagues.co.uk>
2019-03-05 12:26:33 +0000

    Add test for game searching

---
+++ tests/boardgame/test_boardGameSearch.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from unittest.mock import Mock
+
+from boardgamegeek import BGGClient
+
+from boardgame.BoardGame import BoardGameSearch
+
+
+class TestBoardGameSearch(TestCase):
+    def test_search_for_game_less_than_3_chars(self):
+        mock_client = Mock(BGGClient)
+        search = BoardGameSearch(mock_client)
+        self.assertEqual(search.search_for_game(game_name="no"), [])
+        mock_client.search.assert_not_called()
+
+    def test_search_for_game_3_or_more(self):
+        game_name = "dix"
+        expected_data = "data"
+        mock_client = Mock(BGGClient)
+        search = BoardGameSearch(mock_client)
+        mock_client.search.return_value = expected_data
+        self.assertEqual(search.search_for_game(game_name), expected_data)
+        mock_client.search.assert_called_with(game_name, exact=False)
