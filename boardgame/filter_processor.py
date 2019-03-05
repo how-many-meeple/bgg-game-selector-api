ec1e11fe0090f12232b398abb06a49fa06d09406
Teqqles <david.long@racing-leagues.co.uk>
2019-03-05 16:31:44 +0000

    Add ability to optionally filter nothing

--- boardgame/filter_processor.py
+++ boardgame/filter_processor.py
@@ -1,11 +1,15 @@
+from typing import Optional
+
 from boardgamegeek.objects.games import BaseGame
 
-from boardgame import Filter
+from boardgame import filter
 
 
 class FilterProcessor(object):
-    def __init__(self, game_filter: Filter):
+    def __init__(self, game_filter: Optional[filter.Filter]):
         self._filter = game_filter
 
     def filter_game(self, game: BaseGame) -> bool:
+        if not self._filter:
+            return False
         return self._filter.filter(game)
