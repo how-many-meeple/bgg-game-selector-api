ed82e566811cc56779e27c9abd76c3e159b376a9
David Long <david.long@racing-leagues.co.uk>
2026-09-10 08:10:45 +0100

    feat: add VectorCodec for float32 vector encoding

---
+++ src/test/scala/bgg/vector/VectorCodecSpec.scala
@@ -0,0 +1,24 @@
+package bgg.vector
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class VectorCodecSpec extends AnyWordSpec with Matchers:
+
+  "VectorCodec" should:
+    "round-trip a vector within float32 tolerance" in:
+      val original = Vector(0.0, 1.0, 0.5, 0.123456789, -0.25)
+      val decoded = VectorCodec.decode(VectorCodec.encode(original))
+      decoded.isRight shouldBe true
+      val values = decoded.toOption.get
+      values should have size original.size
+      values.zip(original).foreach { case (got, exp) => got shouldBe exp +- 1e-6 }
+
+    "produce 4 bytes per element" in:
+      VectorCodec.encode(Vector.fill(155)(0.5)).length shouldBe 155 * 4
+
+    "round-trip an empty vector" in:
+      VectorCodec.decode(VectorCodec.encode(Vector.empty)) shouldBe Right(Vector.empty)
+
+    "reject bytes whose length is not a multiple of 4" in:
+      VectorCodec.decode(Array[Byte](1, 2, 3)).isLeft shouldBe true
